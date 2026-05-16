f8e2278aa66e59015b8e3be3299238ab946b5068
raja <rajacoder17@gmail.com>
2026-05-16 19:07:28 +0530

    Add new line 16-06-2026

--- script.js
+++ script.js
@@ -7,3 +7,34 @@ menuOpenButton.addEventListener("click",() =>{
 });
 // close the menu when close button is clicked
 menuCloseButton.addEventListener("click",() => menuOpenButton.click());
+
+//Initialize swiper
+const swiper = new Swiper('.slider-wrapper', {
+  // Optional parameters
+  loop: true,
+  spaceBetween:25,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+//   Responsive breakpoints
+  breakpoints:{
+    0:{
+        slidePerView :1
+    },
+    768:{
+        slidePerView :2
+    },
+    1024:{
+        slidePerView :3
+    }
+  }
+});
